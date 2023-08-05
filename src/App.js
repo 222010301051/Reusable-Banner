e4e8964832feb339b93102db0489d6654142d0bc
222010301051 <amattupa@gitam.in>
2023-08-05 06:28:16 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import BannerCardItem from './components/BannerCardItem'
 
 const bannerCardsList = [
   {
@@ -22,6 +23,13 @@ const bannerCardsList = [
   },
 ]
 
-const App = () => <h1>Replace this element with your code</h1>
-
+const App = () => (
+  <div className="bg-container">
+    <ul className="banner-card-list">
+      {bannerCardsList.map(eachcard => (
+        <BannerCardItem bannerCard={eachcard} key={eachcard.id} />
+      ))}
+    </ul>
+  </div>
+)
 export default App
